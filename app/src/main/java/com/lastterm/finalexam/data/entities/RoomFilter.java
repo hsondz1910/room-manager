89a8cf4457a66e07c29cdba4a7d2c783dfdfd9dc
wessley9 <114457599+wessley9@users.noreply.github.com>
2024-12-15 16:29:06 +0700

    update Search
    can search by String, price, area, location

--- app/src/main/java/com/lastterm/finalexam/data/entities/RoomFilter.java
+++ app/src/main/java/com/lastterm/finalexam/data/entities/RoomFilter.java
@@ -1,30 +1,65 @@
 package com.lastterm.finalexam.data.entities;
 
 public class RoomFilter {
-    private double maxPrice;
-    private double minArea;
+    private String search;
+    private long maxPrice;
+    private long minPrice;
+    private long area;
+    private String location;
+
 
     public RoomFilter() {
+        this.search = "";
+        this.minPrice = 0;
+        this.maxPrice = 0;
+        this.area = 0;
+        this.location = "";
     }
 
-    public RoomFilter(double maxPrice, double minArea) {
+    public RoomFilter(String search, long minPrice, long maxPrice, long area, String location) {
+        this.minPrice = minPrice;
         this.maxPrice = maxPrice;
-        this.minArea = minArea;
+        this.area = area;
+        this.location = location;
     }
 
-    public double getMaxPrice() {
+    public long getMaxPrice() {
         return maxPrice;
     }
 
-    public void setMaxPrice(double maxPrice) {
+    public void setMaxPrice(long maxPrice) {
         this.maxPrice = maxPrice;
     }
 
-    public double getMinArea() {
-        return minArea;
+    public long getMinPrice() {
+        return minPrice;
     }
 
-    public void setMinArea(double minArea) {
-        this.minArea = minArea;
+    public void setMinPrice(long minPrice) {
+        this.minPrice = minPrice;
+    }
+
+    public long getArea() {
+        return area;
+    }
+
+    public void setArea(long area) {
+        this.area = area;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getSearch() {
+        return search;
+    }
+
+    public void setSearch(String search) {
+        this.search = search;
     }
 }
