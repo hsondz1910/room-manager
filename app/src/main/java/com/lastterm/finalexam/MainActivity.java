8508a9d6be5a8a190149721bd0faa2b0a0cc7cd9
wessley9 <114457599+wessley9@users.noreply.github.com>
2024-12-23 21:32:53 +0700

    fix interface

--- app/src/main/java/com/lastterm/finalexam/MainActivity.java
+++ app/src/main/java/com/lastterm/finalexam/MainActivity.java
@@ -1,11 +1,13 @@
 package com.lastterm.finalexam;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
@@ -39,9 +41,11 @@ public class MainActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         db = FirebaseFirestore.getInstance();
 
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            Insets navigationBarsInsets = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            View bottomNavigationView = findViewById(R.id.bottom_navigation);
+            bottomNavigationView.setPadding(0, 0, 0, navigationBarsInsets.bottom);
             return insets;
         });
 
